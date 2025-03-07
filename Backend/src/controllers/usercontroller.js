46016275304e5bd621c253768f42ab76dd4510f0
Pahari47 <nachiketapahari00@gmail.com>
2025-03-07 23:29:36 +0530

    hhh

--- Backend/src/controllers/usercontroller.js
+++ Backend/src/controllers/usercontroller.js
@@ -1,9 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "okkk"
-    })
+    
+    const{username, email, password, phone} = req.body
+    console.log("email: ", email);
 })
 
 export default registerUser;
