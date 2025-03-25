a890c3a744bba88c33fe2cbcf264b521102e533c
akash202004 <akashlaha48@gmail.com>
2025-03-25 13:44:21 +0530

    [Backend]: Cloudinary setup complete

--- Backend/src/middlewares/multerConfig.ts
+++ Backend/src/middlewares/multerConfig.ts
@@ -1,35 +1,22 @@
-import multer, { StorageEngine, FileFilterCallback } from 'multer';
-import path from 'path';
-import { Request } from 'express';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinaryConfig";
 
-// Storage configuration
-const storage: StorageEngine = multer.diskStorage({
-    destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
-        cb(null, './src/uploads'); 
-    },
-    filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-    }
+// Cloudinary storage configuration
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (_, file): Promise<{ folder: string; public_id: string; resource_type: string }> => ({
+    folder: "student-documents", 
+    public_id: `${file.fieldname}-${Date.now()}`,
+    resource_type: file.mimetype === "application/pdf" ? "raw" : "auto",
+  }),
 });
 
-// File filter to allow only specific file types
-const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-    const allowedFileTypes = /jpeg|jpg|png|gif|pdf/;
-    const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedFileTypes.test(file.mimetype);
 
-    if (extname && mimetype) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only images and PDFs are allowed'));
-    }
-};
-
-// Multer middleware configuration
+// Multer configuration
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 },
-    fileFilter: fileFilter
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 export default upload;
